33e127e293cb60239164132b033a620d9c245a83
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2024-05-21 12:48:14 -0300

    Invertendo dados com o operador not

---
+++ aula21.py
@@ -0,0 +1,9 @@
+# Operador lógico "not"
+# Usado para inverter expressões
+# not True = False
+# not False = True
+
+# senha = input('Senha: ')
+print(not 1)##inverte o valor true vira falso e vice-versa
+print (not True)
+print (not False)

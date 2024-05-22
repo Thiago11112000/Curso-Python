813849a97511aa9f8d54dcd06512507898eb7429
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2024-05-22 15:36:56 -0300

    alocação de recursos em memória com o id

---
+++ aula29.py
@@ -0,0 +1,17 @@
+"""
+Flag - Marcar um local
+None = Não valor
+is e is not = é ou não é (tipo, valor, identidade)
+id = identidade
+"""
+
+v1 = 'a'
+v2 = 'a'
+print(id(v1))
+print(id(v2))
+#Condição = false
+
+# if condicao:
+#   print('Faça algo')
+# else:
+#   print('Não faça algo')

3483ce26b84c3b0a2fa334d46f7dd61e759f9ac0
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2024-06-03 10:53:59 -0300

    Introdução a tuplas

---
+++ aula56.py
@@ -0,0 +1,14 @@
+"""
+Tipo tupla -- Uma lista imutável
+"""
+nomes = ('Maria', 'Helena', 'Luiz')
+#nomes[1] = 'outro'
+
+print(nomes)
+
+
+#convertendo lista para tupla e vice e versa
+nomes = [ 'Maria', 'Helena', 'Luiz']
+nomes = tuple(nomes)
+nomes = list (nomes)
+print (nomes)
